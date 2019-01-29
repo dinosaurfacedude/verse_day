7c2ac97d1b491e97cfcfdbdf944042556af9b602
dinosaurfacedude <34109000+dinosaurfacedude@users.noreply.github.com>
2019-01-28 23:10:29 -0600

    Add files via upload
    This is just part of the framework. Not even close to having working skeleton code for it.

---
+++ verse_day.java
@@ -0,0 +1,39 @@
+/*
+@author Daniel Jones
+@param IDE used: Atom text editor and linux terminal
+@param OS: Linux Mint 19 XFCE
+@param last edited: 01-28-2019
+@param description: This is a personal program for me and my friends.
+It reads from a text file to get a randomly generated number from 1-365, and
+it then displays a Bible verse based off of that number in JavaFX.
+*/
+
+
+import java.util.*;
+import java.io.*;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+
+public class verse_day extends Application{
+
+  public static void main(String[] args){
+
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+
+
+
+    }
+
+
+
+}
